3f314f7fb826a01cde3dc410b68857b813e205e9
Victor Bitencourt <victorh.biten@gmail.com>
2025-09-30 12:12:44 -0400

    add captura do submit

--- scripts.js
+++ scripts.js
@@ -19,6 +19,29 @@ function formatCurrencyBRL(value) {
         //coloca o valor em formato BRL de currency
 }
 
-form.onsubmit = () => {
+//captura o submit do formulario e armazena as informações criando um novo objeto com os detalhes da despeza
+form.onsubmit = (event) => {
     event.preventDefault()
+    
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+    //chama a função que irá adicionar o item na lista de despezas
+    ExpenseAdd(newExpense)
 }
+
+function ExpenseAdd(newExpense) {
+    try {
+        
+
+    } catch (error){
+        alert("Não foi possível atualizar a lista de despezas.")
+        console.log(error)
+    }
+}
+
