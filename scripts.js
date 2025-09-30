ce7b9087756396bdd89af8eb15b051c1f55a9722
Victor Bitencourt <victorh.biten@gmail.com>
2025-09-30 11:59:37 -0400

    inicital commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const amount = document.getElementById("amount")
+
+amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "") //remove as letras do input
+
+    value = Number(value) / 100 //valor em centavos
+    amount.value = formatCurrencyBRL(value) //aplica o replace no input
+}
+
+function formatCurrencyBRL(value) {
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        })
+        return value
+        //coloca o valor em formato BRL de currency
+}
+
+
+
+
